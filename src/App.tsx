0fd6a849e5e7c37223402d017767d508c4b29608
saramsh <ssarmsh@insightworkshop.io>
2022-08-02 16:45:31 +0545

    basic query done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { gql } from "@apollo/client";
+import React from "react";
+import client from "./config/apolloClient";
+import { GlobalContext } from "./context/globalContext";
+
 function App() {
+  const [state, setState] = React.useState<string>("");
+  const [output, setOutput] = React.useState();
+  const globalContext = React.useContext(GlobalContext);
+  React.useEffect(() => {
+    client
+      .query({
+        query: gql`
+      query GetCountry {
+        country(code: "${globalContext?.userInput}") {
+          __typename
+            name
+            capital
+            emoji
+          }
+      }
+      `,
+      })
+      .then((data: any) => setOutput(data.data))
+      .catch((error) => console.log(error));
+  }, [globalContext?.userInput]);
+
+  const onSubmitHandler = (e: any) => {
+    e.preventDefault();
+    if (globalContext) globalContext?.setUserInput(state);
+  };
   return (
     <div className="App">
       <h2>Graph QL</h2>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" onChange={(e) => setState(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+
+      <p>User Input: {`${globalContext?.userInput}`}</p>
+      <p>{JSON.stringify(output)}</p>
     </div>
   );
 }
